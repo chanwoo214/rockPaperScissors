998789bd1c79842036c1919cfe3b50ce269f189b
Chan Woo SHIM <chanwooshim@Chans-MacBook-Air.local>
2024-08-14 03:54:55 +1200

    alt prop add

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,7 +4,7 @@ const Box = (props) => {
   return (
     <div className='box'>
         <h1>{props.title}</h1>
-        <img className='item-image' src="https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_2.625,f_auto,h_197,q_auto,w_350/c_pad,h_197,w_350/C5099960-06?pgw=1"/>
+        <img className='item-image' src="https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_2.625,f_auto,h_197,q_auto,w_350/c_pad,h_197,w_350/C5099960-06?pgw=1" alt="The image"/>
         <h2>WIN</h2>
     </div>
   );
